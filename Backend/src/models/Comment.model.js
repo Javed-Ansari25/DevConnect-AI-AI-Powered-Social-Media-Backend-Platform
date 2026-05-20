05e8a27a6c59f8df8728e7d2c9c6ba960b142270
Javed Ansari <Javedansari2k09@gmail.com>
2026-05-20 12:59:20 +0530

    Add Comment model with schema definition for owner, post, and text fields

---
+++ Backend/src/models/Comment.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    post: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    },
+
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 500,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
